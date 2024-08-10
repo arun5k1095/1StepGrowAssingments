ff6aa3b1cffdf56224f7cba5e06a63f539e81693
mujtaba-saqib <mujtabasqb@gmail.com>
2024-08-10 11:04:30 +0500

    average

---
+++ Shehryar_assignments2.py
@@ -0,0 +1,7 @@
+prin=int(input("Enter principal amount: "))
+rate=int(input("Enter rate of interest: "))
+time=int(input("Enter time in years: "))
+
+simple_interest=(prin*rate*time)/100
+
+print("The simple interest is ", simple_interest)
