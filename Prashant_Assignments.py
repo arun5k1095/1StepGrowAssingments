24d6d42503939ae8928eafecaaf5be64b400c6cb
PrashantJathan <prashantjathan_09@rediff.com>
2024-08-11 03:21:40 +0530

    Add files via upload

---
+++ Prashant_Assignments.py
@@ -0,0 +1,43 @@
+Statement = "Independence day is on 15th August"
+modified_data = Statement.upper()
+print(modified_data)
+
+Statement = "Independence day is on 15th August"
+modified_data = Statement.replace("I" , "#")
+modified_data1 = Statement.replace("i" , "#")
+modified_data2 = Statement.replace("0" , "#")
+modified_data3 = Statement.replace("u" , "#")
+modified_data4 = Statement.replace("e" , "#")
+modified_data5 = Statement.replace("a" , "#")
+print(modified_data)
+print(modified_data1)
+print(modified_data2)
+print(modified_data3)
+print(modified_data4)
+print(modified_data5)
+
+Email_ID = "prashantjathan_09@rediff.com"
+print(Email_ID[0:14])
+
+
+Statement = "Independence day is on 15th August"
+modified_data = Statement.count("a")
+modified_data1 = Statement.count("e")
+modified_data2 = Statement.count("i")
+modified_data3 = Statement.count("o")
+modified_data4 = Statement.count("u")
+print(modified_data)
+print(modified_data1)
+print(modified_data2)
+print(modified_data3)
+print(modified_data4)
+
+Name = "Prashant Jathan"
+print(Name[::-1])
+
+
+
+
+
+
+
