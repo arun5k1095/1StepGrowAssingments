3b231059b75627d68c7d44b85643e2afc91dab09
priyariyyer <priya.r.iyyer@gmail.com>
2024-08-10 10:02:59 +0400

    Update Priya_Assignments.py

--- Priya_Assignments.py
+++ Priya_Assignments.py
@@ -7,3 +7,14 @@ Science = float(input("Science: "))
 English = float(input("English: "))
 AverageMarks= (Math+Science+English)/3
 print("Your average marks is: ", round(AverageMarks,2))
+
+
+#Assignment 2
+# Write a Python program that takes the principal amount, rate of interest, and time from the user and calculates the simple interest. And print the result.
+
+principal = float(input("What is the deposit amount(Rs): "))
+rate_of_interest = float(input("What is the rate if interest(%): "))
+duration = float(input("What is the deposit duration(yrs): "))
+SI = (principal * rate_of_interest * duration)/100
+
+print("Simple Interest accrued for the deposit terms mentioned is: Rs ", round(SI,2))
