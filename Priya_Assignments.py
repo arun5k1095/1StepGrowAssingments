861ee7b8b9b3887c40a6046eb525d17b490898db
priyariyyer <priya.r.iyyer@gmail.com>
2024-08-11 07:58:41 +0400

    Update Priya_Assignments.py

--- Priya_Assignments.py
+++ Priya_Assignments.py
@@ -18,3 +18,40 @@ duration = float(input("What is the deposit duration(yrs): "))
 SI = (principal * rate_of_interest * duration)/100
 
 print("Simple Interest accrued for the deposit terms mentioned is: Rs ", round(SI,2))
+
+
+#Assignment 3
+# Ask user to enter a long sentence. Convert the whole sentence into upper case and print the result.
+sentence = input("Please enter a long sentence : ")
+print("Your sentence in upper case is shown as: ", sentence.upper())
+
+
+# Ask user to enter his full name , reverse the full name and print it.
+full_name = input("Please enter your full name : ")
+print("Your name in reverse order is: ", full_name[: :-1])
+
+
+#Ask user to enter a long sentence , print only the first half of the sentence.
+sentence = input("Please enter a long sentence : ")
+end = int(len(sentence)/2)
+print("Half of the sentence entered is shown as: ", sentence[:end:])
+
+
+# Ask user to enter a long sentence , Replace all the occurences of vowels with # , and print the modified content.
+sentence = input("Please enter a long sentence: ")
+print("Your sentence replacing all vowels with # is shown as: ", sentence.replace("a","#").replace("e","#").replace("i","#").replace("o","#").replace("o","#").replace("u","#"))
+
+
+# Ask user to enter his email id , fetch the user name from the string and print it.
+email = input("Please enter your email address: ")
+end = email.index("@")
+print("Your name extracted from email address is: ", email[:end:])
+
+# Ask user to enter a long sentence , count the total number of Vowels that are present in the sentence.
+sentence = input("Please enter a long sentence: ")
+vowels = sentence.count("a")
+vowels = vowels + sentence.count("e")
+vowels = vowels + sentence.count("i")
+vowels = vowels + sentence.count("o")
+vowels = vowels + sentence.count("u")
+print("Count of vowels in your sentence is: ", vowels)
