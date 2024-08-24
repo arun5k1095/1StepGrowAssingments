0896976ee1c7deaef8819d5a4d4c9bb69fdb904f
Sreedhar <sreedhar@apples-MacBook-Pro.local>
2024-08-24 10:22:33 +0530

    even_odd

--- odd_even.py
+++ odd_even.py
@@ -2,9 +2,11 @@ temperatures = [-1,50,6,-46,-20,81,74,41,24,-60,-2,17,23,28,57,29,-16,-4,29,9,11
 even = []
 odd = []
 for temp in temperatures:
-    if temp % 2 == 0:
+    if temp % 2 == 0 and temp > 0:
         even.append(temp)
-    else:
+    elif temp % 2 != 0 and temp > 0:
         odd.append(temp)
+    else:
+        continue
 print("Even temperatures: ", even)
 print("Odd temperatures: ", odd)
